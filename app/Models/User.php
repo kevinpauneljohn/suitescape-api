ad9675ef700c420303482971b3c1913ffe5dc88a
melmatx <melpalana13@gmail.com>
2023-09-04 18:26:26 +0800

    Modified User model for new columns

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,9 +21,13 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'firstname',
+        'middlename',
+        'lastname',
         'email',
+        'mobile_number',
         'password',
+        'date_of_birth',
     ];
 
     /**
