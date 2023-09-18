f1341b5d1f7c4e687cf5276db3a1b6b56bae8f40
melmatx <melpalana13@gmail.com>
2023-09-19 00:56:46 +0800

    Created Video Migration table

---
+++ database/migrations/2023_09_15_131348_create_videos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('user_id');
+            $table->string('filename');
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->integer('likes')->default(0);
+            $table->enum('privacy', ['public', 'private'])->default('private');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('videos');
+    }
+};
