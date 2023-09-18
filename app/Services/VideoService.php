24d1156e44d6002c8084c41b4e7cd7a18e15ee8a
melmatx <melpalana13@gmail.com>
2023-09-19 00:50:06 +0800

    Created VideoService.php

---
+++ app/Services/VideoService.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Video;
+use Exception;
+use Illuminate\Http\UploadedFile;
+use Micilini\VideoStream\VideoStream;
+
+class VideoService
+{
+    public function getFileName(): string
+    {
+        return date("d-m-Y-H-i-s") . "_" . auth()->user()->email . "_" . uniqid();
+    }
+
+    public function uploadVideo(UploadedFile $video): string
+    {
+        $filename = $this->getFileName() . $video->getClientOriginalExtension();
+        $video->storeAs("videos", $filename, 'public');
+        return $filename;
+    }
+
+    public function streamVideo(Video $video)
+    {
+        $localUrl = storage_path('app/videos/' . $video['filename']);
+        $options = [
+            'is_localPath' => true
+        ];
+
+        try {
+            return (new VideoStream())->streamVideo($localUrl, $options);
+        } catch (Exception $exception) {
+            return response()->json([
+                "message" => $exception->getMessage(),
+            ], 404);
+        }
+    }
+}
