823fe5a884b044023d5caf4fbf3de43f185baed9
melmatx <melpalana13@gmail.com>
2023-09-22 16:38:43 +0800

    Created room categories table

---
+++ database/migrations/2023_09_19_011634_create_room_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('room_categories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignUuid('listing_id');
+            $table->string('name');
+            $table->integer('size');
+            $table->json('type_of_beds');
+            $table->integer('pax');
+            $table->float('price');
+            $table->float('tax');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('room_categories');
+    }
+};
