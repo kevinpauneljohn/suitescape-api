80dc51168ad3e4dea2d9441e80c51899e2b6d1ef
melmatx <melpalana13@gmail.com>
2023-09-24 14:04:51 +0800

    Added saves and views endpoint, also added route names

--- routes/api.php
+++ routes/api.php
@@ -19,13 +19,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
-    });
+    })->name("user");
 });
 
 Route::post('/register', [RegistrationController::class, "register"])->name("register");
 Route::post('/login', [RegistrationController::class, "login"])->name("login");
-Route::post('/logout', [RegistrationController::class, "logout"]);
+Route::post('/logout', [RegistrationController::class, "logout"])->name("logout");
 
-Route::post('/videos', [VideoController::class, "uploadVideo"]);
-Route::get('/videos/{id}', [VideoController::class, "getVideo"]);
-Route::get('/videos', [VideoController::class, "getAllVideos"]);
+Route::post('/videos', [VideoController::class, "uploadVideo"])->name("videos.upload");
+Route::get('/videos', [VideoController::class, "getAllVideos"])->name("videos.all");
+Route::get('/videos/{id}', [VideoController::class, "streamVideo"])->name("videos.stream");
+Route::post('/videos/{id}/like', [VideoController::class, "likeVideo"])->name("videos.like");
+Route::post('/videos/{id}/save', [VideoController::class, "saveVideo"])->name("videos.save");
