32097348e42d45de71ba7147a8347b2c60322cc9
melmatx <melpalana13@gmail.com>
2023-09-24 14:13:08 +0800

    Created filter for public videos to Video model

--- app/Services/VideoRetrievalService.php
+++ app/Services/VideoRetrievalService.php
@@ -8,7 +8,7 @@ class VideoRetrievalService
 {
     public function getAllPublicVideos()
     {
-        return Video::where('privacy', 'public')
+        return Video::public()
             ->orderBy('created_at', 'desc')
             ->orderBy('id', 'desc')
             ->with('listing')
