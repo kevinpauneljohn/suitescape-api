1cc5d4005961fc252191f20eb3ff3e9bc8399c22
melmatx <melpalana13@gmail.com>
2023-11-18 09:25:47 +0800

    Created bookings migration table

---
+++ database/migrations/2023_11_04_012800_create_bookings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->foreignUuid('user_id');
+            $table->foreignUuid('coupon_id');
+            $table->decimal('amount');
+            $table->enum('status', ['upcoming', 'ongoing', 'cancelled', 'completed']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
