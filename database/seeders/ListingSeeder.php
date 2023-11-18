f4179c35e2c3b70342e337062fe72a1fc86a2ef4
melmatx <melpalana13@gmail.com>
2023-11-18 09:13:53 +0800

    Removed unused import for ListingSeeder.php

--- database/seeders/ListingSeeder.php
+++ database/seeders/ListingSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Listing;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class ListingSeeder extends Seeder
