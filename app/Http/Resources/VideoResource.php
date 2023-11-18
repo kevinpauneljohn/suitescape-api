03d8583e82125db784d57830afdc87c65df7e539
melmatx <melpalana13@gmail.com>
2023-11-18 10:12:38 +0800

    Created resource for Video model

---
+++ app/Http/Resources/VideoResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class VideoResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'filename' => $this->filename,
+            'privacy' => $this->privacy,
+            'listing_id' => $this->whenNotNull($this->whenLoaded('listing', null, $this->listing_id)),
+            'listing' => new ListingResource($this->whenLoaded('listing')),
+            'url' => route('api.videos.get', $this->id, false),
+        ];
+    }
+}
