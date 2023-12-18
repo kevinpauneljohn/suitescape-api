c8360b0f1e571a88ea290e75132c5a707b0ac49b
melmatx <melpalana13@gmail.com>
2023-12-18 15:04:21 +0800

    Created PasswordUpdateRequest.php

---
+++ app/Http/Requests/PasswordUpdateRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+
+class PasswordUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'new_password.different' => 'The new password must be different from the current password.',
+        ];
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'current_password' => ['required', 'current_password:sanctum'],
+            'new_password' => ['required', 'bail', 'confirmed', 'different:current_password',
+                Password::min(8)->letters()->mixedCase()->numbers()->symbols(),
+            ],
+        ];
+    }
+}
