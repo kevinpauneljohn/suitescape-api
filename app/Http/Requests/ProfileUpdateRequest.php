4eaa296f1ad56c0fd6c3418b223510140db77210
melmatx <melpalana13@gmail.com>
2024-01-28 16:16:22 +0800

    Added profile picture upload

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -30,6 +30,18 @@ class ProfileUpdateRequest extends FormRequest
     }
 
     /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'mobile_number.phone' => 'The mobile number is invalid.',
+        ];
+    }
+
+    /**
      * Get the validation rules that apply to the request.
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
@@ -40,13 +52,14 @@ class ProfileUpdateRequest extends FormRequest
 
         return array_merge(Arr::except($registrationRequest->rules(), 'password'), [
             'email' => ['required', 'email', Rule::unique('users', 'email')->ignore($this->user()?->id)],
-            'mobile_number' => ['nullable', 'string', Rule::unique('users', 'mobile_number')->ignore($this->user()?->id), 'regex:/^(?:\+\d{1,3}\s?|0)\d{4,14}$/'],
+            'mobile_number' => ['nullable', 'string', 'phone:INTERNATIONAL,PH', Rule::unique('users', 'mobile_number')->ignore($this->user()?->id)],
             'gender' => ['nullable', 'string', 'in:male,female,other'],
             'address' => ['required', 'string'],
             'zipcode' => ['required', 'string', 'regex:/^\d{4,5}$/'],
             'city' => ['required', 'string'],
             'region' => ['required', 'string'],
             'date_of_birth' => ['sometimes', 'date', 'before:18 years ago'],
+            'picture' => ['nullable', 'image'],
         ]);
     }
 }
