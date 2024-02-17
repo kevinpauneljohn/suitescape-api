251b517cafef3bdd62dfc152938aaaff8f874483
melmatx <melpalana13@gmail.com>
2024-02-17 09:33:03 +0800

    Created resource for Message model

---
+++ app/Http/Resources/MessageResource.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MessageResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $user = $request->user('sanctum');
+
+        return [
+            'id' => $this->id,
+            $this->mergeUnless($this->relationLoaded('chat'), [
+                'chat_id' => $this->chat_id,
+            ]),
+            $this->mergeUnless($this->relationLoaded('sender'), [
+                'sender_id' => $this->sender_id,
+            ]),
+            $this->mergeUnless($this->relationLoaded('receiver'), [
+                'receiver_id' => $this->receiver_id,
+            ]),
+            'chat' => new ChatResource($this->whenLoaded('chat')),
+            'sender' => new UserResource($this->whenLoaded('sender')),
+            'receiver' => new UserResource($this->whenLoaded('receiver')),
+            'content' => $this->content,
+            'read_at' => $this->read_at,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'is_current_user_sender' => $user && $this->sender_id === $user->id,
+        ];
+    }
+}
