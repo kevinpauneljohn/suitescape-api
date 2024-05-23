05819d9871253b129e00d8139239d3cb137ba701
melmatx <melpalana13@gmail.com>
2024-05-23 11:38:54 +0800

    Refactor ReviewResource to remove room relationship

--- app/Http/Resources/ReviewResource.php
+++ app/Http/Resources/ReviewResource.php
@@ -16,12 +16,10 @@ class ReviewResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            $this->mergeUnless($this->relationLoaded('room'), ['room_id' => $this->room_id]),
             $this->mergeUnless($this->relationLoaded('user'), ['user_id' => $this->user_id]),
             $this->mergeUnless($this->relationLoaded('listing'), ['listing_id' => $this->listing_id]),
             'content' => $this->content,
             'rating' => $this->rating,
-            'room' => new RoomResource($this->whenLoaded('room')),
             'user' => new UserResource($this->whenLoaded('user')),
             'listing' => new ListingResource($this->whenLoaded('listing')),
             'created_at' => $this->created_at,
