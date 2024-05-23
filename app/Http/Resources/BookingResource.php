fed15950d0a593e92f13f193d98f92bc84cc5b7e
melmatx <melpalana13@gmail.com>
2024-05-23 10:41:41 +0800

    Added new columns to BookingResource.php

--- app/Http/Resources/BookingResource.php
+++ app/Http/Resources/BookingResource.php
@@ -16,12 +16,26 @@ class BookingResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'coupon' => new CouponResource($this->whenLoaded('coupon')),
+            $this->mergeUnless($this->relationLoaded('listing'), ['listing_id' => $this->listing_id]),
             $this->mergeUnless($this->relationLoaded('coupon'), ['coupon_id' => $this->coupon_id]),
+            'listing' => new ListingResource($this->whenLoaded('listing')),
+            'coupon' => new CouponResource($this->whenLoaded('coupon')),
+            'invoice' => new InvoiceResource($this->whenLoaded('invoice')),
             'booking_rooms' => BookingRoomResource::collection($this->whenLoaded('bookingRooms')),
+            'booking_addons' => BookingAddonResource::collection($this->whenLoaded('bookingAddons')),
             'amount' => $this->amount,
             'message' => $this->message,
             'status' => $this->status,
+            'date_start' => $this->date_start,
+            'date_end' => $this->date_end,
+            'cancellation_fee' => $this->cancellation_fee,
+
+            $this->mergeWhen($this->cancellation_policy, fn () => [
+                'cancellation_policy' => $this->cancellation_policy,
+            ]),
+            $this->mergeWhen($this->suitescape_cancellation_fee, fn () => [
+                'suitescape_cancellation_fee' => $this->suitescape_cancellation_fee,
+            ]),
             //            'created_at' => $this->created_at,
         ];
     }
