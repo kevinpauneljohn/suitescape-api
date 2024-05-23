dcb8e33cfcb4faf2b5dba7405b7083f6530aab09
melmatx <melpalana13@gmail.com>
2024-05-23 11:40:12 +0800

    Refactor ProfileUpdateRequest to extend RegisterUserRequest and update validation rules

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -2,12 +2,11 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 use Illuminate\Validation\Rule;
 
-class ProfileUpdateRequest extends FormRequest
+class ProfileUpdateRequest extends RegisterUserRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -36,9 +35,9 @@ class ProfileUpdateRequest extends FormRequest
      */
     public function messages(): array
     {
-        return [
+        return array_merge(parent::messages(), [
             'mobile_number.phone' => 'The mobile number is invalid.',
-        ];
+        ]);
     }
 
     /**
@@ -48,18 +47,17 @@ class ProfileUpdateRequest extends FormRequest
      */
     public function rules(): array
     {
-        $registrationRequest = RegisterUserRequest::createFrom($this);
-
-        return array_merge(Arr::except($registrationRequest->rules(), 'password'), [
+        return array_merge(Arr::except(parent::rules(), 'password'), [
             'email' => ['required', 'email', Rule::unique('users', 'email')->ignore($this->user()?->id)],
             'mobile_number' => ['nullable', 'string', 'phone:INTERNATIONAL,PH', Rule::unique('users', 'mobile_number')->ignore($this->user()?->id)],
             'gender' => ['nullable', 'string', 'in:male,female,other'],
             'address' => ['required', 'string'],
-            'zipcode' => ['required', 'string', 'regex:/^\d{4,5}$/'],
+            'zipcode' => ['nullable', 'string', 'regex:/^\d{4,5}$/'],
             'city' => ['required', 'string'],
             'region' => ['required', 'string'],
             'date_of_birth' => ['sometimes', 'date', 'before:18 years ago'],
-            'picture' => ['nullable', 'image'],
+            'profile_image' => ['nullable', 'image'],
+            'cover_image' => ['nullable', 'image'],
         ]);
     }
 }
