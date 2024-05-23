4ac17a9849351b93534dc43c86229149654e8ebc
melmatx <melpalana13@gmail.com>
2024-05-23 11:39:28 +0800

    Refactor HostResource to include 'listings_avg_rating' metric

--- app/Http/Resources/HostResource.php
+++ app/Http/Resources/HostResource.php
@@ -15,11 +15,12 @@ class HostResource extends JsonResource
     public function toArray(Request $request): array
     {
         $hostMetrics = [
+            'reviews' => ReviewResource::collection($this->whenLoaded('listingsReviews')),
+            'listings' => ListingResource::collection($this->whenLoaded('listings')),
             'listings_count' => $this->whenCounted('listings'),
             'listings_likes_count' => $this->whenCounted('listingsLikes'),
             'listings_reviews_count' => $this->whenCounted('listingsReviews'),
-            'listings' => ListingResource::collection($this->whenLoaded('listings')),
-            'reviews' => ReviewResource::collection($this->whenLoaded('listingsReviews')),
+            'listings_avg_rating' => $this->whenAggregated('listingsReviews', 'rating', 'avg', fn ($value) => round($value, 1)),
         ];
 
         return array_merge((new UserResource($this))->resolve(), $hostMetrics);
