4486b504c85ff13a5243e5854ea38f2556b94020
melmatx <melpalana13@gmail.com>
2024-05-23 10:46:43 +0800

    Update RoomCategorySeeder to retrieve listings with multiple rooms

--- database/seeders/RoomCategorySeeder.php
+++ database/seeders/RoomCategorySeeder.php
@@ -15,7 +15,7 @@ class RoomCategorySeeder extends Seeder
     {
         $categories = ['Single', 'Double', 'Triple', 'Queen', 'King', 'Twin', 'Hollywood Twin Room', 'Double-double', 'Studio', 'Suite', 'Mini Suite', 'President Suite', 'Apartments', 'Connecting Rooms', 'Murphy Room', 'Accessible Room', 'Cabana', 'Adjoining Rooms', 'Adjacent Rooms', 'Villa', 'Executive Floor', 'Smoking Room'];
 
-        $listings = Listing::all();
+        $listings = Listing::hasMultipleRooms()->get();
 
         foreach ($listings as $listing) {
             foreach ($categories as $category) {
