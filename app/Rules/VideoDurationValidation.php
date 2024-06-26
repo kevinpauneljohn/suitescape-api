2625e484462f9069c16cb29eac14888f766fb852
rrrrrcadalzo <rcadalzo@spcf.edu.ph>
2024-06-26 23:39:19 +0800

    Removed FFMPEG temporarily due to server limitations

--- app/Rules/VideoDurationValidation.php
+++ app/Rules/VideoDurationValidation.php
@@ -4,7 +4,7 @@ namespace App\Rules;
 
 use Closure;
 use Illuminate\Contracts\Validation\ValidationRule;
-use ProtoneMedia\LaravelFFMpeg\Support\FFMpeg;
+use Owenoj\LaravelGetId3\GetId3;
 
 class VideoDurationValidation implements ValidationRule
 {
@@ -22,26 +22,29 @@ class VideoDurationValidation implements ValidationRule
      * Run the validation rule.
      *
      * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     *
+     * @throws \getid3_exception
      */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        $video = FFMpeg::open($value);
-
-        if (! $video->isVideo()) {
-            $fail("The {$attribute} must be a video file.");
-
-            return;
-        }
+        //        $video = FFMpeg::open($value);
+        //
+        //        if (! $video->isVideo()) {
+        //            $fail("The {$attribute} must be a video file.");
+        //
+        //            return;
+        //        }
 
         // Get the duration of the video in seconds
-        $duration = $video->getDurationInSeconds();
+        //        $duration = $video->getDurationInSeconds();
+        $duration = GetId3::fromUploadedFile($value)->getPlaytimeSeconds();
 
         if ($duration < $this->minDuration) {
-            $fail("The {$attribute} must be at least {$this->minDuration} seconds.");
+            $fail("The $attribute must be at least $this->minDuration seconds.");
         }
 
         if ($this->maxDuration && $duration > $this->maxDuration) {
-            $fail("The {$attribute} may not be greater than {$this->maxDuration} seconds.");
+            $fail("The $attribute may not be greater than $this->maxDuration seconds.");
         }
     }
 }
