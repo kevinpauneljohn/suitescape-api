cd3886cc7c53090f65ed31131dd93e2a6aaf993a
melmatx <melpalana13@gmail.com>
2024-07-13 15:24:33 +0800

    Customized email markdown template with logo

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,13 @@
+@props(['url'])
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+@if (trim($slot) === config('app.name'))
+<img src={{ asset('storage/suitescape-logo.png') }} class="logo" alt="{{ config('app.name') }} Logo" onerror="this.style.display='none'; document.getElementById('fallback').style.display='inline';">
+<span id="fallback" style="display:none;">{{ $slot }}</span>
+@else
+{{ $slot }}
+@endif
+</a>
+</td>
+</tr>
