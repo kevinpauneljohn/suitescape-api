219b0167faee63d604ecc93c540974f0a204f755
melmatx <melpalana13@gmail.com>
2025-02-27 16:06:23 +0800

    Added new fields for payout methods

--- database/migrations/2025_01_02_082437_create_payout_methods_table.php
+++ database/migrations/2025_01_02_082437_create_payout_methods_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('payout_methods', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->foreignUuid('user_id');
+            $table->uuidMorphs('payoutable');
+            $table->boolean('is_default')->default(false);
+            $table->string('status')->default('active');
             $table->timestamps();
         });
     }
